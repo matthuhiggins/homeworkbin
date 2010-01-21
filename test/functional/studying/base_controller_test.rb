edd39f82fdce368fa3e5cea6a41a848f3d305b93
Matthew Higgins <matthuhiggins@roflcopter.local>
2010-01-20 20:01:08 -0800

    tests for base controllers

---
+++ test/functional/studying/base_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class Studying::BaseControllerTest < ActionController::StudentTestCase
+  class Controller < Studying::BaseController
+    def index
+      render :text => 'foo'
+    end
+  end
+  
+  tests Controller
+  
+  def test_authentication
+    get :index
+    assert_redirected_to login_path
+  end
+end
