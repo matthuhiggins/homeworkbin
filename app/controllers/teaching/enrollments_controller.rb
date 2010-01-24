8ffeb00baada047ecad33629e505a317a9feb12d
Matthew Higgins <matthuhiggins@roflcopter.local>
2010-01-24 11:35:00 -0800

    the show must go on

--- app/controllers/teaching/enrollments_controller.rb
+++ app/controllers/teaching/enrollments_controller.rb
@@ -1,7 +1,11 @@
 class Teaching::EnrollmentsController < Teaching::BaseController
+  def index
+    @enrollments = current_course.enrollments
+  end
+
   def create
     current_course.enrollments.matriculate_emails params[:emails]
     flash[:notice] = "Good work"
-    redirect_to 'http://www.google.com'
+    redirect_to teaching_enrollments_path(current_course)
   end
 end
---
+++ test/functional/teaching/enrollments_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class Teaching::EnrollmentsControllerTest < ActionController::TeacherTestCase
+  def test_index
+    teacher_get :index
+    assert_not_nil assigns(:enrollments)
+  end
+  
+  def test_create
+    teacher_post :create, :emails => 'sam@u.washington.edu, joe@yahoo.com'
+    
+    assert_equal 2, current_course.enrollments.size
+    assert_redirected_to teaching_enrollments_path(current_course)
+  end
+end
