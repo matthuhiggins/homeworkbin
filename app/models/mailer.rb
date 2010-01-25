b74d99b5c85e372f8f243bd9cf262bc5713b20d1
Matthew Higgins <matthuhiggins@roflcopter.local>
2010-01-24 18:44:34 -0800

    add from address

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,23 +1,27 @@
 class Mailer < ActionMailer::Base
   def registration registration
+    from        %{"Homework Bin" <no-reply@homeworkbin.com>}
     recipients  registration.email
     subject     'Confirm your email address'
     body        :registration => registration
   end
   
   def enrollment enrollment
+    from        %{"Homework Bin" <no-reply@homeworkbin.com>}
     recipients  enrollment.email
     subject     "Confirm your enrollment in #{enrollment.course.name}"
     body        :enrollment => enrollment
   end
   
   def lost_password lost_password
+    from        %{"Homework Bin" <no-reply@homeworkbin.com>}
     recipients  lost_password.person.email
     subject     "Homework Bin password change"
     body        :lost_password => lost_password
   end
   
   def assignment assignment, student
+    from        %{"Homework Bin" <no-reply@homeworkbin.com>}
     recipients  student.email
     subject     "New assignment in #{assignment.course.name}"
     body        :assignment => assignment, :student => student
