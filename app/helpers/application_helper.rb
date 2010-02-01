48a9afa0b27a03d3fcfb3c196bdb076a4a357e3a
Matthew Higgins <matthuhiggins@roflcopter.local>
2010-02-01 11:26:01 -0800

    focus fields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def title(title)
     content_for(:title) { ' - ' + title }
   end
+  
+  def focus(dom_id)
+    content_tag :script, "document.getElementById('#{dom_id}').focus();"
+  end
 end
--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -5,4 +5,9 @@ class ApplicationHelperTest < ActionView::TestCase
     title "hello world"
     assert_equal " - hello world", @content_for_title
   end
+  
+  def test_focus
+    script = focus 'foo'
+    assert_dom_equal "<script>document.getElementById('foo').focus();", script
+  end
 end
