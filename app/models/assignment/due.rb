6e1b4276e0df995511e4138129837373f8e565c4
Matthew Higgins <matthuhiggins@roflcopter.local>
2010-02-06 12:52:54 -0800

    Add tests and assignment.due_at

--- app/models/assignment/due.rb
+++ app/models/assignment/due.rb
@@ -2,12 +2,25 @@ class Assignment
   module Due
     def self.included base
       base.class_eval do
-        # attr_accessor :due_minutes
         validates_inclusion_of :due_minutes, :in => 0..1440
         
         extend ActiveRecord::DateValidation
         validates_date_format :due_date
       end
+    end
+
+    def late?
+      
+    end
+
+    def due_at
+      Time.mktime(
+        due_date.year,
+        due_date.month,
+        due_date.day,
+        due_minutes / 60,
+        due_minutes % 60
+      )
     end
     
     private
@@ -20,13 +33,5 @@ class Assignment
       #     due_minutes % 60
       #   )
       # end
-      
-      def converted_due_minutes
-        
-      end
-      
-      def converted_due_date
-        @converted_due_date ||= due_date.is_a?(Date) ? due_date : Date.strptime(due_date, '%m/%d/%Y')
-      end
   end
 end
--- test/unit/assignment/due_test.rb
+++ test/unit/assignment/due_test.rb
@@ -4,7 +4,17 @@ class Assignment::DueTest < ActiveSupport::TestCase
   include ActiveRecord::DateValidationTests
   test_date_format_validation :due_date
   
-  def test_minutes_range
-    
+  def test_minutes_validation
+    assert Factory.build(:assignment, :due_minutes => -1).invalid?
+    assert Factory.build(:assignment, :due_minutes => 1441).invalid?
+    assert Factory.build(:assignment, :due_minutes => 0).valid?
+    assert Factory.build(:assignment, :due_minutes => 1440).valid?
+  end
+  
+  def test_due_at
+    assert_equal(
+      Factory.build(:assignment, :due_date => '05/22/2004', :due_minutes => 122).due_at,
+      Time.mktime(2004, 5, 22, 2, 2)
+    )
   end
 end
