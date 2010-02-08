27761b0638815a9772081f684af9236acae37113
Matthew Higgins <matthuhiggins@roflcopter.local>
2010-02-07 20:41:47 -0800

    Add a hour select options

--- app/helpers/time_helper.rb
+++ app/helpers/time_helper.rb
@@ -2,4 +2,19 @@ module TimeHelper
   def assignment_due_at(assignment)
     assignment.due_date.strftime '%A, %B %d'
   end
+  
+  def hour_options
+    options = []
+
+    (0..23).each do |hour|
+      meridian = hour < 12 ? 'am' : 'pm'
+      display_hour = hour % 12
+      display_hour = 12 if display_hour == 0
+      [0, 30].each do |minutes|
+        options << ["#{display_hour}:#{minutes.to_s.rjust 2, '0'}#{meridian}", hour * 60 + minutes]
+      end
+    end
+
+    options
+  end
 end
--- test/unit/helpers/time_helper_test.rb
+++ test/unit/helpers/time_helper_test.rb
@@ -4,4 +4,12 @@ class TimeHelperTest < ActionView::TestCase
   def test_assignment_due_at
     assert_equal 'Saturday, May 21', assignment_due_at(Factory :assignment, :due_date => Date.new(2005, 5, 21))
   end
+  
+  def test_hour_options
+    options = hour_options
+    
+    assert_equal 48, options.size
+    assert options.include?(['11:00am', 660])
+    assert options.include?(['2:30pm', 870])
+  end
 end
