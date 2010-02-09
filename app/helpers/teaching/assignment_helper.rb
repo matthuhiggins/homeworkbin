cc297fe37e5e1a313871ff5ffbc258f6acb57609
Matthew Higgins <matthuhiggins@roflcopter.local>
2010-02-08 22:02:25 -0800

    Give a default date for assignments

--- app/helpers/teaching/assignment_helper.rb
+++ app/helpers/teaching/assignment_helper.rb
@@ -1,4 +1,12 @@
 module Teaching::AssignmentHelper
+  def assignment_due_date(assignment)
+    if assignment.due_date.present?
+      assignment.due_date.strftime '%m/%d/%Y'
+    else
+      Date.current.strftime '%m/%d/%Y'
+    end
+  end
+  
   def assignment_due_minutes(assignment)
     if assignment.due_minutes.present?
       assignment.due_minutes
--- test/unit/helpers/teaching/assignment_helper_test.rb
+++ test/unit/helpers/teaching/assignment_helper_test.rb
@@ -1,18 +1,11 @@
 require 'action_view_test'
 
 class Teaching::AssignmentHelperTest < ActionView::TestCase
-  def test_due_minutes_uses_existing_value
-    assignment = Factory :assignment, :due_minutes => 120
-
-    assert 120, assignment_due_minutes(assignment)
+  def test_due_minutes_without_last
+    assert 120, assignment_due_minutes(Factory.build(:assignment, :due_minutes => 120))
+    assert_equal 12 * 60, assignment_due_minutes(Factory.build(:assignment, :due_minutes => nil))
   end
 
-  def test_due_minutes_default_without_last
-    assignment = Factory :assignment, :due_minutes => nil
-    
-    assert_equal 12 * 60, assignment_due_minutes(assignment)
-  end
-  
   def test_due_minutes_defaults_to_last
     course = Factory :course
     Factory :assignment, :course => course, :due_minutes => 120
@@ -20,5 +13,10 @@ class Teaching::AssignmentHelperTest < ActionView::TestCase
     assignment = course.assignments.new
     
     assert_equal 120, assignment_due_minutes(assignment)
+  end
+  
+  def test_due_date
+    assert_equal '12/25/2005', assignment_date_date(Factory.build(:assignment, :due_date => '12/25/2005'))
+    assert_equal Date.current.strftime('%m/%d/%Y'), assignment_date_date(Factory.build(:assignment, :due_date => nil))
   end
 end
