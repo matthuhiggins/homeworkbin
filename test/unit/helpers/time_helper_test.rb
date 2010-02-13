6b5c8cc72d7274c857a8fe575014f602db66ee6b
Matthew Higgins <matthuhiggins@roflcopter.local>
2010-02-13 11:22:18 -0800

    Add tests

--- test/unit/helpers/time_helper_test.rb
+++ test/unit/helpers/time_helper_test.rb
@@ -9,7 +9,15 @@ class TimeHelperTest < ActionView::TestCase
     assert options.include?(['2:30pm', 870])
   end
 
-  def test_date_format_div
-    
+  def test_date_format_div_shows_error
+    course = Factory.build :course, :start_date => 'xx/yy/lolz'
+    assert course.invalid?
+    assert_dom_equal "<div class='formError'>must be mm/dd/yyyy</div>", date_format_div(course, :start_date)
+  end
+  
+  def test_date_format_div_shows_tip
+    course = Factory.build :course
+    assert course.valid?
+    assert_dom_equal "<div class='tip'>mm/dd/yyyy</div>", date_format_div(course, :start_date)
   end
 end
