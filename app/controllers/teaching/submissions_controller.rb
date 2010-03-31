837d3a60eb3e98ae2c4d74d10bc1750676b145a3
Matthew <developer@matthewhiggins.com>
2010-03-30 17:20:41 -0700

    dead stuff

--- app/controllers/teaching/submissions_controller.rb
+++ app/controllers/teaching/submissions_controller.rb
@@ -1,10 +1,6 @@
 class Teaching::SubmissionsController < Teaching::BaseController
   topic 'homework'
   
-  def show
-    render :layout => 'paper'
-  end
-  
   private
     def current_submission
       @current_submission ||= current_course.submissions.find params[:id]
