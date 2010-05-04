8d89e2875a119d8a2140548ff7485587bafbfb63
Matthew <developer@matthewhiggins.com>
2010-05-03 23:12:26 -0700

    Add test for the text fields

--- test/unit/composition_test.rb
+++ test/unit/composition_test.rb
@@ -5,10 +5,21 @@ class CompositionTest < ActiveRecord::TestCase
   test_denormalization :studier, :student
   test_denormalization :assignment, :course
   
-  def test_ungraded
+  def test_grade_scopes
     graded = factory.create graded_at: Time.current
     ungraded = factory.create graded_at: nil
     
     assert_equal [ungraded], Composition.ungraded
+  end
+
+  def test_annotated_saved_as_original
+    composition = factory.create :original => 'foo'
+    assert_equal 'foo', composition.annotated
+
+    composition.update_attribute :annotated, 'bar'
+    assert_equal 'bar', composition.annotated
+
+    composition.update_attribute :original, 'faz'
+    assert_equal 'faz', composition.annotated
   end
 end
